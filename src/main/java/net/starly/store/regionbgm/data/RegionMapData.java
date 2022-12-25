88800de55141e39051a6ceda5f2b98d014caf08c
idkNicks <yangdaeyeong0808@gmail.com>
2022-12-25 22:57:16 +0900

    Update Project

---
+++ src/main/java/net/starly/store/regionbgm/data/RegionMapData.java
@@ -0,0 +1,9 @@
+package net.starly.store.regionbgm.data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegionMapData {
+
+    protected static Map<String, String> bgmMap = new HashMap<>();
+}
