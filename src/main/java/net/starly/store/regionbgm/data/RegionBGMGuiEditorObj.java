34b074349048a57f686950eb8d690fb3b0c6d5b0
yangdaeyeong <yangdaeyeong0808@gmail.com>
2022-12-27 08:41:57 +0900

    Update Project

--- src/main/java/net/starly/store/regionbgm/data/RegionBGMGuiEditorObj.java
+++ src/main/java/net/starly/store/regionbgm/data/RegionBGMGuiEditorObj.java
@@ -4,6 +4,7 @@ import net.starly.core.data.Config;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.Inventory;
@@ -18,9 +19,17 @@ import static net.starly.store.regionbgm.data.RegionMapData.guiEditorMap;
 public class RegionBGMGuiEditorObj {
 
     private Player player;
+    private String name;
 
 
-    public RegionBGMGuiEditorObj(Player player) { this.player = player; }
+    public RegionBGMGuiEditorObj(Player player) {
+        this.player = player;
+    }
+
+    public RegionBGMGuiEditorObj(String name, Player player) {
+        this.name = name;
+        this.player = player;
+    }
 
 
     /**
@@ -32,17 +41,50 @@ public class RegionBGMGuiEditorObj {
                 config.getConfig().getInt("edit_gui_settings.size") * 9,
                 config.getConfig().getString("edit_gui_settings.title") + " " + region);
 
+        statusEditor(inv);
         lengthEditor(inv);
         volumeEditor(inv);
         pitchEditor(inv);
-        loopEditor(inv);
+
+        Config bgm = new Config("bgm", plugin);
+        ConfigurationSection section = bgm.getConfig().getConfigurationSection("bgm." + region);
+
+        if(section.getBoolean("loop")) {
+            loopTrueEditor(inv);
+        } else {
+            loopFalseEditor(inv);
+        }
 
         player.openInventory(inv);
     }
 
 
     /**
+     * 구역브금 편집기 - 상태
+     *
+     * @param inv 편집기 인벤토리
+     */
+    private void statusEditor(Inventory inv) {
+        Config config = new Config("config", plugin);
+
+        String name = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("edit_gui_settings.status.name"));
+        String material = config.getConfig().getString("edit_gui_settings.status.material");
+        List<String> lore = Translate.color(config.getConfig().getStringList("edit_gui_settings.status.lore"));
+        int slot = config.getConfig().getInt("edit_gui_settings.status.slot");
+
+        ItemStack item = new ItemStack(Material.valueOf(material));
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+
+        setItem(inv, name, material, lore, slot);
+    }
+
+
+    /**
      * 구역브금 편집기 - 브금 길이
+     *
      * @param inv 편집기 인벤토리
      */
     private void lengthEditor(Inventory inv) {
@@ -50,7 +92,7 @@ public class RegionBGMGuiEditorObj {
 
         String name = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("edit_gui_settings.length.name"));
         String material = config.getConfig().getString("edit_gui_settings.length.material");
-        List<String> lore =  Translate.color(config.getConfig().getStringList("edit_gui_settings.length.lore"));
+        List<String> lore = Translate.color(config.getConfig().getStringList("edit_gui_settings.length.lore"));
         int slot = config.getConfig().getInt("edit_gui_settings.length.slot");
 
         ItemStack item = new ItemStack(Material.valueOf(material));
@@ -65,6 +107,7 @@ public class RegionBGMGuiEditorObj {
 
     /**
      * 구역브금 편집기 - 브금 불륨
+     *
      * @param inv 편집기 인벤토리
      */
     private void volumeEditor(Inventory inv) {
@@ -72,7 +115,7 @@ public class RegionBGMGuiEditorObj {
 
         String name = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("edit_gui_settings.volume.name"));
         String material = config.getConfig().getString("edit_gui_settings.volume.material");
-        List<String> lore =  Translate.color(config.getConfig().getStringList("edit_gui_settings.volume.lore"));
+        List<String> lore = Translate.color(config.getConfig().getStringList("edit_gui_settings.volume.lore"));
         int slot = config.getConfig().getInt("edit_gui_settings.volume.slot");
 
         ItemStack item = new ItemStack(Material.valueOf(material));
@@ -87,6 +130,7 @@ public class RegionBGMGuiEditorObj {
 
     /**
      * 구역브금 편집기 - 브금 높낮이
+     *
      * @param inv 편집기 인벤토리
      */
     private void pitchEditor(Inventory inv) {
@@ -108,16 +152,16 @@ public class RegionBGMGuiEditorObj {
 
 
     /**
-     * 구역브금 편집기 - 브금 반복 여부
+     * 구역브금 편집기 - 브금 반복 여부 | 켜져있을 때
      * @param inv 편집기 인벤토리
      */
-    private void loopEditor(Inventory inv) {
+    private void loopTrueEditor(Inventory inv) {
         Config config = new Config("config", plugin);
 
-        String name = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("edit_gui_settings.loop.name"));
-        String material = config.getConfig().getString("edit_gui_settings.loop.material");
-        List<String> lore = Translate.color(config.getConfig().getStringList("edit_gui_settings.loop.lore"));
-        int slot = config.getConfig().getInt("edit_gui_settings.loop.slot");
+        String name = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("edit_gui_settings.loop.true.name"));
+        String material = config.getConfig().getString("edit_gui_settings.loop.true.material");
+        List<String> lore = Translate.color(config.getConfig().getStringList("edit_gui_settings.loop.true.lore"));
+        int slot = config.getConfig().getInt("edit_gui_settings.loop.true.slot");
 
         ItemStack item = new ItemStack(Material.valueOf(material));
         ItemMeta meta = item.getItemMeta();
@@ -127,15 +171,38 @@ public class RegionBGMGuiEditorObj {
 
         setItem(inv, name, material, lore, slot);
     }
-    
-    
+
+
+    /**
+     * 구역브금 편집기 - 브금 반복 여부 | 꺼져있을 때
+     * @param inv 편집기 인벤토리
+     */
+    private void loopFalseEditor(Inventory inv) {
+        Config config = new Config("config", plugin);
+
+        String name = ChatColor.translateAlternateColorCodes('&', config.getConfig().getString("edit_gui_settings.loop.false.name"));
+        String material = config.getConfig().getString("edit_gui_settings.loop.false.material");
+        List<String> lore = Translate.color(config.getConfig().getStringList("edit_gui_settings.loop.false.lore"));
+        int slot = config.getConfig().getInt("edit_gui_settings.loop.false.slot");
+
+        ItemStack item = new ItemStack(Material.valueOf(material));
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+
+        setItem(inv, name, material, lore, slot);
+    }
+
+
     /**
      * ItemStack을 저장합니다.
-     * @param inv 인벤토리
-     * @param name 아이템 이름
+     *
+     * @param inv      인벤토리
+     * @param name     아이템 이름
      * @param material 아이템 종류
-     * @param lore 아이템 로어
-     * @param slot 아이템 슬롯
+     * @param lore     아이템 로어
+     * @param slot     아이템 슬롯
      */
     private void setItem(Inventory inv, String name, String material, List<String> lore, int slot) {
 
@@ -146,18 +213,19 @@ public class RegionBGMGuiEditorObj {
         itemStack.setItemMeta(itemMeta);
         inv.setItem(slot, itemStack);
     }
-    
+
 
     /**
      * 구역브금 길이를 편집합니다
+     *
      * @param event 플레이어가 채팅을 입력했을 때 발생하는 이벤트
      */
     public void editBGMLength(AsyncPlayerChatEvent event) {
 
         String message = event.getMessage();
 
-        if(guiEditorMap.containsKey(player)) {
-            if(guiEditorMap.get(player) == GuiEditor.LENGTH) {
+        if (guiEditorMap.containsKey(player)) {
+            if (guiEditorMap.get(player) == GuiEditor.LENGTH) {
                 event.setCancelled(true);
                 player.sendMessage("§a구역브금 길이를 " + message + "초로 설정했습니다.");
                 guiEditorMap.remove(player);
